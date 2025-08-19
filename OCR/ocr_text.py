12340b5229836e31560e644d16b97cad518396b3
nehz <andrewzhenwork@gmail.com>
2025-08-19 15:38:58 -0700

    EXIF processing for ocr_text.py
    Added PIL exif transpose to have correct photo orientation.

--- OCR/ocr_text.py
+++ OCR/ocr_text.py
@@ -2,6 +2,10 @@ from google.cloud import vision
 from google.oauth2 import service_account
 from ocr_parser import parse_contact_lens_data
 
+# image pre-processing libraries
+from PIL import Image, ImageOps
+from io import BytesIO
+
 # Path to your service account JSON key file
 KEY_PATH = "../credentials/google_vision_key.json"
 
@@ -12,8 +16,21 @@ def detect_text_with_service_account(image_path):
     # Create the Vision API client with credentials
     client = vision.ImageAnnotatorClient(credentials=credentials)
 
-    with open(image_path, "rb") as image_file:
-        content = image_file.read()
+
+    # Open image and fix orientation based on EXIF
+    # NOTE: previewed images may look right side up
+    img = Image.open(image_path)
+    img = ImageOps.exif_transpose(img)
+
+    # Convert to bytes for Vision API 
+    # Alternatively save the images again and then use the with open
+    img_bytes = BytesIO()
+    img.save(img_bytes, format="JPEG")
+    content = img_bytes.getvalue()
+
+    # with open(image_path, "rb") as image_file:
+    #     content = image_file.read()
+
 
     image = vision.Image(content=content)
     
