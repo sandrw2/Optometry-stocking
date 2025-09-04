68ad6046ed477f80c2139f63f05b9edc048c275a
nehz <andrewzhenwork@gmail.com>
2025-09-04 16:29:16 -0700

    modified tests 21 to 39

--- OCR_test/test/test_ocr.py
+++ OCR_test/test/test_ocr.py
@@ -6,7 +6,7 @@ import os
 
 # Add project root (one level above OCR_test) to Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-from OCR import find_title_keywords#, find_brand_and_line#, match_best_line 
+from OCR import find_title_keywords, find_brand_and_line#, match_best_line 
 # ====================================================================================
 
 import pytest
@@ -37,25 +37,25 @@ test_cases = [
     ("test_018.jpeg", ["ACUVUE", "OASYS", "1-DAY", "HYDRALUXE", "ASTIGMATISM"]),
     ("test_019.jpeg", ["PRECISION1", "ALCON", "ASTIGMATISM"]),
     ("test_020.jpeg", ["COMFILCON", "TORIC"]),
-    ("test_021.jpeg", ["ACUVUE", "OASYS", "HYDRACLEAR", "ASTIGMATISM"]),
-    ("test_022.jpeg", ["DAILIES", "TOTAL1", "ALCON"]),
-    ("test_023.jpeg", ["1-DAY", "TORIC", "SILICONE"]),
-    ("test_024.jpeg", ["PRECISION1", "ALCON"]),
-    ("test_025.jpeg", ["ACUVUE", "OASYS", "HYDRACLEAR", "ASTIGMATISM"]),
-    ("test_026.jpeg", ["DAILIES", "TOTAL1", "ALCON"]),
-    ("test_027.jpeg", ["ACUVUE", "OASYS", "1-DAY", "HYDRALUXE", "ASTIGMATISM"]),
-    ("test_028.jpeg", ["ACUVUE", "OASYS", "MAX", "1-DAY", "MULTIFOCAL"]),
-    ("test_029.jpeg", ["COMFILCON", "XR", "TORIC"]),
-    ("test_030.jpeg", ["ACUVUE", "OASYS", "1-DAY", "HYDRALUXE", "ASTIGMATISM"]),
-    ("test_031.jpeg", ["1-DAY", "TORIC", "SILICONE"]),
-    ("test_032.jpeg", ["ACUVUE", "OASYS", "HYDRACLEAR"]),
-    ("test_033.jpeg", ["PRECISION1", "ALCON"]),
-    ("test_034.jpeg", ["TOTAL30", "ASTIGMATISM", "ALCON"]),
-    ("test_035.jpeg", ["ACUVUE", "OASYS", "HYDRACLEAR", "ASTIGMATISM"]),
-    ("test_036.jpeg", ["1-DAY", "MULTIFOCAL", "SILICONE"]),
-    ("test_037.jpeg", ["COMFILCON", "TORIC"]),
-    ("test_038.jpeg", ["BAUSCH+LOMB", "ULTRA", "MULTIFOCAL", "ASTIGMATISM"]),
-    ("test_039.jpeg", ["PRECISION1", "ALCON"])
+    ("test_021.jpeg", ("ACUVUE", "ACUVUE OASYS 2-Week")),
+    ("test_022.jpeg", ("ALCON", "DAILIES TOTAL1")),
+    ("test_023.jpeg", ("COOPERVISION", "MYDAY TORIC")),
+    ("test_024.jpeg", ("ALCON", "PRECISION1")),
+    ("test_025.jpeg", ("ACUVUE", "ACUVUE OASYS 2-Week FOR ASTIGMATISM")),
+    ("test_026.jpeg", ("ALCON", "DAILIES TOTAL1")),
+    ("test_027.jpeg", ("ACUVUE", "ACUVUE OASYS 1-DAY FOR ASTIGMATISM")),
+    ("test_028.jpeg", ("ACUVUE", "ACUVUE OASYS MAX 1-DAY MULTIFOCAL")),
+    ("test_029.jpeg", ("COOPERVISION", "BIOFINITY TORIC XR")),
+    ("test_030.jpeg", ("ACUVUE", "ACUVUE OASYS 1-DAY FOR ASTIGMATISM")),
+    ("test_031.jpeg", ("COOPERVISION", "MYDAY TORIC")),
+    ("test_032.jpeg", ("ACUVUE", "ACUVUE OASYS 2-Week")),
+    ("test_033.jpeg", ("ALCON", "PRECISION1")),
+    ("test_034.jpeg", ("ALCON", "TOTAL30 FOR ASTIGMATISM")),
+    ("test_035.jpeg", ("ACUVUE","ACUVUE OASYS 2-Week FOR ASTIGMATISM")),
+    ("test_036.jpeg", ("COOPERVISION", "MYDAY MULTIFOCAL")),
+    ("test_037.jpeg", ("COOPERVISION", "BIOFINITY TORIC")),
+    ("test_038.jpeg", ("BAUSCH+LOMB", "ULTRA MULTIFOCAL FOR ASTIGMATISM")),
+    ("test_039.jpeg", ("ALCON", "PRECISION1"))
 
 ]
 
@@ -65,6 +65,7 @@ def test_extract_value(image_path, expected):
     text = all_results.get(image_path, [])
     test_data = [x[0] for x in text]
     result = find_title_keywords(test_data)
+    result = find_brand_and_line(result)
     assert set(result) == set(expected)
 
 # def test_extracting_title_keywords():
